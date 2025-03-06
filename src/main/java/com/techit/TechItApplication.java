729cbfe05b459389e536be799d11c499211ee177
sulbin <tlsdnr1135@naver.com>
2025-03-06 22:14:14 +0900

    init

---
+++ src/main/java/com/techit/TechItApplication.java
@@ -0,0 +1,13 @@
+package com.techit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechItApplication.class, args);
+	}
+
+}
