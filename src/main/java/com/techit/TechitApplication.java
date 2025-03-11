56bdbf6da57a3b94086c150a2d80983591d61ca3
swnam <tlsdnr1135@naver.com>
2025-03-11 12:08:02 +0900

    init

---
+++ src/main/java/com/techit/TechitApplication.java
@@ -0,0 +1,13 @@
+package com.techit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/techit/TechitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
