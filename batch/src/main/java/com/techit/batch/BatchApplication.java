83e0895ef4802dda03f83988051601c882a346b5
swnam <tlsdnr1135@naver.com>
2025-03-11 18:21:11 +0900

    init multi-module scanBasePackages 등록.

--- batch/src/main/java/com/techit/batch/BatchApplication.java
+++ batch/src/main/java/com/techit/batch/BatchApplication.java
@@ -3,7 +3,7 @@ package com.techit.batch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.techit.common","com.techit.batch"})
 public class BatchApplication {
 
     public static void main(String[] args) {
--- llm/src/main/java/com/techit/llm/LlmApplication.java
+++ llm/src/main/java/com/techit/llm/LlmApplication.java
@@ -3,7 +3,7 @@ package com.techit.llm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.techit.common","com.techit.llm"})
 public class LlmApplication {
 
     public static void main(String[] args) {
